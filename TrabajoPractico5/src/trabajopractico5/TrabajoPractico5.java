522ab6ea2ba3dec621bb5cd520acb96f75e25318
Tomas Abatedaga Biole <abatedagatomas@gmail.com>
2025-09-07 00:06:08 -0300

    cree vista principal y vista agregar cliente

---
+++ TrabajoPractico5/src/trabajopractico5/TrabajoPractico5.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajopractico5;
+
+import Vistas.MenuPrincipal;
+
+/**
+ *
+ * @author abate
+ */
+public class TrabajoPractico5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MenuPrincipal.main(args);
+    }
+    
+}
